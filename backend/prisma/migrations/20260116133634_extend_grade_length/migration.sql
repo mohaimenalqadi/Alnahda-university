1ee1d91031e6c6a4f39e81ba14c62d9a5efce894
mohaimenalqadi <abdulmuhayminalqadhi@gmail.com>
2026-01-16 15:39:02 +0200

    fix: increase grade length limit and resolve course update error

---
+++ backend/prisma/migrations/20260116133634_extend_grade_length/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "grade_history" ALTER COLUMN "letter_grade" SET DATA TYPE VARCHAR(50);
+
+-- AlterTable
+ALTER TABLE "grades" ALTER COLUMN "letter_grade" SET DATA TYPE VARCHAR(50);
